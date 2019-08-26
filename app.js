0a4910ef34fc4bce1cc20a2b86803289b745ed14
Pawid-prianto27 <pawitanto27@gmail.com>
2019-08-26 13:32:54 +0700

    createanddelete directory

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 var fs = require('fs');
 
+fs.mkdirSync('layout'); // create directori
 
-
-fs.writeFileSync('test2.txt', 'contoh write file'); // ini contoh writeFile
-
-var readMe = fs.readFileSync('readMe.txt', 'utf8'); // ini contoh readfile
-var test2 = fs.readFileSync('test2.txt', 'utf8'); // ini contoh readfile
-
-
-console.log(readMe, test2);
+fs.rmdirSync('views') // delete directori
