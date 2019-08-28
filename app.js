da3aedd8eb5593b9d43f4effaa4031dfb87769bf
Pawid-Priyanto <pawitanto27@gmail.com>
2019-08-28 14:03:01 +0700

    belajar polindrum

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-var fs = require('fs');
+// easy way
+function isPolindrum(str) {
+  return str === str.split('').reverse().join('');
+}
 
-fs.mkdirSync('layout'); // create directori
 
-fs.rmdirSync('views') // delete directori
+// cara ribet
+function isPolindrum(str) {
+  return str.split('').every((char,i) => {
+    return char === str[str.length -i -1];
+  });
+}
+
+console.log(isPolindrum('anna')); // true
+console.log(isPolindrum('annabe')); // false
+console.log(isPolindrum('pa abccba ap')); //guest what?
+
